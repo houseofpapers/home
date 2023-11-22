691982839161efdafacbed456013477721379285
Veer Mehta <72297798+Veer2906@users.noreply.github.com>
2023-11-22 19:14:10 +0530

    Clean Up
    Removed stuff we are not going to use for Beta Roll Out

---
+++ Resoruce Scripts/rename.py
@@ -0,0 +1,22 @@
+import os
+
+def rename_pdfs(parent_folder):
+    for folder_name in os.listdir(parent_folder):
+        folder_path = os.path.join(parent_folder, folder_name)
+
+        if os.path.isdir(folder_path):
+            first_two_words = ' '.join(folder_name.split()[:2])
+
+            for filename in os.listdir(folder_path):
+                if filename.endswith('.pdf'):
+                    old_filepath = os.path.join(folder_path, filename)
+
+                    new_filename = f"{first_two_words}_{filename}"
+
+                    new_filepath = os.path.join(folder_path, new_filename)
+
+                    os.rename(old_filepath, new_filepath)
+                    print(f"Renamed: {filename} to {new_filename}")
+
+parent_folder_path = r'C:\Users\veera\Desktop\Renaming'
+rename_pdfs(parent_folder_path)
