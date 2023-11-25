41a392f38675f66bb405b798bc680f2c41e4614e
Rishi Mody <rishimody13@hotmail.com>
2023-11-25 18:28:24 +0530

    sike sl hl

--- Resource Scripts/path_extractor.py
+++ Resource Scripts/path_extractor.py
@@ -1,5 +1,5 @@
 import os
-sessions = ["2023 May"]
+sessions = ["2001 May", "2001 November", "2002 May", "2002 November", "2003 May", "2003 November", "2004 May", "2004 November", "2005 May", "2005 November", "2006 May", "2006 November", "2007 May", "2007 November", "2008 November", "2009 November", "2010 May", "2010 November", "2011 May", "2011 November", "2012 May", "2012 November", "2013 May", "2013 November", "2014 May", "2014 November", "2015 May", "2015 November", "2016 May", "2016 November", "2017 May", "2017 November", "2018 May", "2018 November", "2019 May", "2019 November", "2020 November", "2021 May", "2021 November", "2022 May", "2022 November"]
 #reverse order of sessions in above list
 sessions.reverse()
 
@@ -11,7 +11,7 @@ def list_pdf_files(folder_path):
       return pdf_files
 for session in sessions:
   # Replace 'your/desktop/folder/path' with the path to your desktop folder
-  desktop_folder_path = r'C:\Users\Rishi Mody\Documents\GitHub\Trial-Website\pdfs\Economics_SL' + '\\' + session + ' Examination Session'
+  desktop_folder_path = r'C:\Users\Rishi Mody\Documents\GitHub\Trial-Website\pdfs\Psychology_HL' + '\\' + session + ' Examination Session'
   
   pdf_files = list_pdf_files(desktop_folder_path)
   
@@ -21,7 +21,7 @@ for session in sessions:
   for i in range(len(pdf_files)):
       div_str += ''' <p class="paper-title"><a href="PATH" target="_blank">DESCRIPTOR</a></p>'''
       div_str+="\n"
-      div_str = div_str.replace("PATH", "pdfs/Economics_SL" + '/' + session + " Examination Session/" + pdf_files[i])
+      div_str = div_str.replace("PATH", "pdfs/Psychology_HL" + '/' + session + " Examination Session/" + pdf_files[i])
       
 
 
@@ -38,72 +38,80 @@ for session in sessions:
         path = ""
       elif "French" in path:
         path = ""
-      elif "paper_1_TZ1_SL_markscheme" in path or "paper_1__TZ1_SL_markscheme" in path:
-        DESCRIPTOR = "Paper 1 TZ1 SL Markscheme"
+      elif "paper_1_HLSL_markscheme" in path or "paper_1__HLSL_markscheme" in path:
+        DESCRIPTOR = "Paper 1 HL/SL Markscheme"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_1_TZ1_SL" in path or "paper_1__TZ1_SL" in path:
-        DESCRIPTOR = "Paper 1 TZ1 SL"
+      elif "paper_1_HLSL" in path or "paper_1__HLSL" in path:
+        DESCRIPTOR = "Paper 1 HL/SL"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_1_TZ2_SL_markscheme" in path or "paper_1__TZ2_SL_markscheme" in path:
-        DESCRIPTOR = "Paper 1 TZ2 SL Markscheme"
+      elif "paper_1_TZ1_HL_markscheme" in path or "paper_1__TZ1_HL_markscheme" in path:
+        DESCRIPTOR = "Paper 1 TZ1 HL Markscheme"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_1_TZ2_SL" in path or "paper_1__TZ2_SL" in path:
-        DESCRIPTOR = "Paper 1 TZ2 SL"
+      elif "paper_1_TZ1_HL" in path or "paper_1__TZ1_HL" in path:
+        DESCRIPTOR = "Paper 1 TZ1 HL"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_1__SL_markscheme" in path or "paper_1_SL_markscheme" in path:
-        DESCRIPTOR = "Paper 1 SL Markscheme"
+      elif "paper_1_TZ2_HL_markscheme" in path or "paper_1__TZ2_HL_markscheme" in path:
+        DESCRIPTOR = "Paper 1 TZ2 HL Markscheme"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_1__SL" in path or "paper_1_SL" in path:
-        DESCRIPTOR = "Paper 1 SL"
+      elif "paper_1_TZ2_HL" in path or "paper_1__TZ2_HL" in path:
+        DESCRIPTOR = "Paper 1 TZ2 HL"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_2_reading_comprehension__question_booklet_SL" in path:
-        DESCRIPTOR = "Paper 2 SL Question Booklet"
+      elif "paper_1__HL_markscheme" in path or "paper_1_HL_markscheme" in path:
+        DESCRIPTOR = "Paper 1 HL Markscheme"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_2_reading_comprehension__text_booklet_SL" in path:
-        DESCRIPTOR = "Paper 2 SL Text Booklet"
+      elif "paper_1__HL" in path or "paper_1_HL" in path:
+        DESCRIPTOR = "Paper 1 HL"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_2_reading_comprehension__SL_markscheme" in path:
-        DESCRIPTOR = "Paper 2 SL Markscheme"   
+      elif "paper_2_reading_comprehension__question_booklet_HL" in path:
+        DESCRIPTOR = "Paper 2 HL Question Booklet"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "listening_comprehension__SL_markscheme" in path:
-        DESCRIPTOR = "Listening Comprehension SL Markscheme"
+      elif "paper_2_reading_comprehension__text_booklet_HL" in path:
+        DESCRIPTOR = "Paper 2 HL Text Booklet"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "listening_comprehension__SL" in path:
-        DESCRIPTOR = "Listening Comprehension SL"
+      elif "paper_2_reading_comprehension__HL_markscheme" in path:
+        DESCRIPTOR = "Paper 2 HL Markscheme"   
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_1_question_booklet_SL" in path:
-        DESCRIPTOR = "Paper 1 SL Question Booklet"
+      elif "listening_comprehension__HL_markscheme" in path:
+        DESCRIPTOR = "Listening Comprehension HL Markscheme"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_1_text_booklet_SL" in path:
-        DESCRIPTOR = "Paper 1 SL Text Booklet"
+      elif "listening_comprehension__HL" in path:
+        DESCRIPTOR = "Listening Comprehension HL"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_2_SL_markscheme" in path or "paper_2__SL_markscheme" in path:
-        DESCRIPTOR = "Paper 2 SL Markscheme"
+      elif "paper_1_question_booklet_HL" in path:
+        DESCRIPTOR = "Paper 1 HL Question Booklet"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_2_SL" in path or "paper_2__SL" in path:
-        DESCRIPTOR = "Paper 2 SL"
+      elif "paper_1_text_booklet_HL" in path:
+        DESCRIPTOR = "Paper 1 HL Text Booklet"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_3_SL_markscheme" in path or "paper_3__SL_markscheme" in path:
-        DESCRIPTOR = "Paper 3 SL Markscheme"
+      elif "paper_2_HL_markscheme" in path or "paper_2__HL_markscheme" in path:
+        DESCRIPTOR = "Paper 2 HL Markscheme"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
-      elif "paper_3_SL" in path or "paper_3__SL" in path:
-        DESCRIPTOR = "Paper 3 SL"
+      elif "paper_2_HL" in path or "paper_2__HL" in path:
+        DESCRIPTOR = "Paper 2 HL"
+        path = path.replace("DESCRIPTOR", DESCRIPTOR)
+        new_paths.append(path)
+      elif "paper_3_HL_markscheme" in path or "paper_3__HL_markscheme" in path:
+        DESCRIPTOR = "Paper 3 HL Markscheme"
+        path = path.replace("DESCRIPTOR", DESCRIPTOR)
+        new_paths.append(path)
+      elif "paper_3_HL" in path or "paper_3__HL" in path:
+        DESCRIPTOR = "Paper 3 HL"
         path = path.replace("DESCRIPTOR", DESCRIPTOR)
         new_paths.append(path)
         
