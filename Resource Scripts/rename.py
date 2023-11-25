5c8ddf4e32f470daec1a40295ff3ae528c8929cb
Veer Mehta <72297798+Veer2906@users.noreply.github.com>
2023-11-25 11:47:15 +0530

    latest

--- Resource Scripts/rename.py
+++ Resource Scripts/rename.py
@@ -18,5 +18,5 @@ def rename_pdfs(parent_folder):
                     os.rename(old_filepath, new_filepath)
                     print(f"Renamed: {filename} to {new_filename}")
 
-parent_folder_path = r'C:\Users\veera'
+parent_folder_path = r'C:\Users\veera\Desktop\Veer\Coding\HOLR Website\Papers\Geography\Geography'
 rename_pdfs(parent_folder_path)
